680b3bbf74e6ffd3a1689764f47178b0453558c1
Daniel Wiesmann <daniel.wiesmann@tecnico.ulisboa.pt>
2016-05-02 06:31:45 +0000

    version 0.2.2

--- R/readRasterFolder.R
+++ R/readRasterFolder.R
@@ -12,15 +12,20 @@ function(path, samplename = 'sample', filenames = NULL, object = new('rasclass')
 		samplename <- substr(samplename, 1, nchar(samplename)-4)
 	}
 	object@samplename <- samplename
-	
+
 	# Clean and store path
 	if(substr(path,nchar(path), nchar(path)) != '/'){
 		path <- paste(path,'/', sep='')
 	}
 	object@path <- path
 
-	# Read Variable Names
+	# Remember current working directory and set it back on exit, then
+	# change to data folder.
+	userwd <- getwd()
+	on.exit(setwd(userwd))
 	setwd(path)
+
+	# Read Variable Names
 	if(length(filenames) == 0){
 		filelist <- Sys.glob('*.asc')
 		filelist <- filelist[filelist != paste(object@samplename, '.asc' , sep='')]
@@ -39,15 +44,15 @@ function(path, samplename = 'sample', filenames = NULL, object = new('rasclass')
 
 	# Store file names as names for data columns
 	namelist <- substr(filelist, 1, nchar(filelist)-4)
-	
+
 	# Load Sample
 	cat('\nReading Raster grids from "', getwd(), '"\n', sep ='')
 	cat(paste(paste(object@samplename),'.asc',sep=''), '\n')
 
 	thissample  <- readRaster(paste(object@samplename, '.asc', sep=''), asInteger = asInteger)
 	object@data <- data.frame(thissample@grid)
-	
-	# Set gridSkeleton headers 
+
+	# Set gridSkeleton headers
 	object@gridSkeleton@ncols 		<- thissample@ncols
 	object@gridSkeleton@nrows 		<- thissample@nrows
 	object@gridSkeleton@xllcorner 	<- thissample@xllcorner
@@ -55,16 +60,16 @@ function(path, samplename = 'sample', filenames = NULL, object = new('rasclass')
 	object@gridSkeleton@cellsize 	<- thissample@cellsize
 	object@gridSkeleton@NAvalue 	<- thissample@NAvalue
 	rm(thissample)
-	
+
 	# Load Dependent Varialbe Data using Filenames
 	for(i in 1:length(filelist)){
-		
+
 		# Communicate file to read
 		cat(filelist[i], '\n')
 
 		# Load data from file
 		tempraster <- readRaster(filelist[i], asInteger = asInteger)
-		
+
 		# Check if header is equal to sample
 		if( object@gridSkeleton@nrows     != tempraster@nrows |
 			object@gridSkeleton@ncols     != tempraster@ncols |
@@ -72,7 +77,7 @@ function(path, samplename = 'sample', filenames = NULL, object = new('rasclass')
 			object@gridSkeleton@yllcorner != tempraster@yllcorner |
 			object@gridSkeleton@cellsize  != tempraster@cellsize
 			){ stop('The input raster grids do not all have the same header') }
-		
+
 		# Update gridSkeleton and drop uncomplete rows
 		if(i == 1){
 			# Add data to dataframe
@@ -88,17 +93,17 @@ function(path, samplename = 'sample', filenames = NULL, object = new('rasclass')
 			# Update gridSkeleton
 			object@gridSkeleton@grid <- as.integer(object@gridSkeleton@grid * !is.na(tempraster@grid))
 		}
-		
+
 		# Remove rows with NAs in the new column
 		object@data <- object@data[!is.na(object@data[, i+1]), ]
 	}
-	
+
 	# Set Names in Data Frame
 	names(object@data) <- append(object@samplename, namelist)
 
 	# Build Formula
 	object <- buildFormula(object)
-	
+
 	# Return object
 	object
 }
